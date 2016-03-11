a647ccb92eed503b46fa17a3a71e5a0b5ae9e11e
1nthought <anna.burdette@covenant.edu>
2016-03-10 20:17:55 -0700

    2nd stage

--- messages.php
+++ messages.php
@@ -122,7 +122,8 @@ if ($action == "create") {
     include('templates/delete.messages.php');   
 } elseif ($action == "do_create") {
 
-    move_uploaded_file($_FILES['attachment']['tmp_name'], __DIR__ ."/storage/".basename($_FILES['attachment']['name']));
+    move_uploaded_file($_FILES['attachment']['tmp_name'], __DIR__ ."/storage/".$name = basename($_FILES['attachment']['name']));
+    
 
     # Prep data
     $P_subj = makeSemiSafe($_REQUEST['shbj']);
