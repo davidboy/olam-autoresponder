35b641aa3aa2fb4979b8783a3f283b552db87325
David Reed <dboyreed@charter.net>
2016-03-11 14:00:14 -0700

    Fix copyright information typo

--- mailchecker.php
+++ mailchecker.php
@@ -1,6 +1,6 @@
 <?php
 // This file is part of Olam Autoresponder.
-// Copyright (c) 2014-2016 Aaron Colman and Adaptive Business Design.
+// Copyright (c) 2004-2007 Aaron Colman and Adaptive Business Design.
 // Copyright (c) 2016 Anna Burdette, Benjamin Jobson, and David Reed.
 //
 // Olam Autoresponder is free software: you can redistribute it and/or modify
